b365d7f9229458917f42656a74d7a5cd9d2abb1d
Steven Bunde <stevenbunde2020@gmail.com>
2023-12-08 21:19:28 -0500

    Updated users url in homepage url

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -116,7 +116,7 @@ export default function Homepage() {
 
   const handleSignup = async (username, password, email) => {
     try { 
-      const res = await fetch("http://localhost:3000/api/users", {
+      const res = await fetch("/api/users", {
         method: "POST",
         body: JSON.stringify({ username, password, email }),
         headers: {
